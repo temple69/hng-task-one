1b36b171df9be6655f24322b4359b9d26ff3cbab
temple <utemple169@gmail.com>
2022-10-30 06:54:05 +0100

    This is the Links section

---
+++ src/components/Links/Links.jsx
@@ -0,0 +1,15 @@
+import './links.css'
+const Links = () => {
+  return (
+    <ul>
+        <li><a href="https://twitter.com/temple_snowden" id='twitter'>Twitter Link</a></li>
+        <li><a href="https://training.zuri.team/" id="btn__zuri">Zuri Link</a></li>
+        <li><a href="http://books.zuri.team" id="books">Zuri Books</a></li>
+        <li><a href="https://books.zuri.team/python-for-beginners?ref_id=temple" id="book__python">Python Books</a></li>
+        <li><a href="https://background.zuri.team" id="pitch">Background Check For Coders</a></li>
+        <li><a href="https://books.zuri.team/design-rules" id="book__design">Design Books</a></li>
+    </ul>
+  )
+}
+
+export default Links
