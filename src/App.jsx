e79f7a41c1cea0d5b92107129c84ba3a0a036a77
temple <utemple169@gmail.com>
2022-10-30 06:58:41 +0100

    View where components are imported into and rendered on the browser

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Links from "./components/Links/Links";
+import Profile from "./components/Profile/Profile";
+
+
+function App() {
+    return (  
+        <>
+        <Profile/>
+        <Links/>
+        </>
+    );
+}
+
+export default App;
